9a1f8d508762c0c09676c1f2b1d8a6056dc0ad81
MYspacedge <myspacedge@gmail.com>
2017-12-16 13:33:55 +0300

    Java Temel Örnekler

---
+++ ikisayitoplami.java
@@ -0,0 +1,10 @@
+package ikisayitoplami;
+
+public class ikisayitoplami {
+      public static void main(String[] args) {
+        // TODO code application logic here
+        int s1=10,s2=20,sonuc;
+        sonuc=s1+s2;
+        System.out.println(s1+"+"+s2+"="+sonuc);
+    }
+}
---
+++ merhabadunya.java
@@ -0,0 +1,9 @@
+package javaapplication1;
+
+public class merhabadunya {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Merhaba MYspacedge");
+    }    
+}
